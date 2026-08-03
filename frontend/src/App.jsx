ca0e53f635123d67ca85e239e54f7d3b082f9190
youtoto-nor <rebufq890@gmail.com>
2026-08-03 17:02:59 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import LoginPage from "./features/login/LoginPage";
-
+import "./App.css";
+import logo from "./logo.svg";
 function App() {
   return (
-    <LoginPage />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://react.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
 export default App;
